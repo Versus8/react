d19cdbbd9d60f35555a84c71787b53455535ec45
Veronique Tureau <veronique.tureau@gmail.com>
2024-02-07 09:46:50 -0500

    Learning React States

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 // import componentsImg from "./assets/components.png" => inutile maintenant que l'on importe le fichier data.js
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data-with-examples.js"
 
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+    // console.log(selectedTopic);
+  }
+
+  let tabContent = "Please select a topic."
+
+  if (selectedTopic) {
+    tabContent =
+      <div id='tab-content'>
+        <h3>{ EXAMPLES[selectedTopic].title }</h3>
+        <p>{ EXAMPLES[selectedTopic].description }</p>
+        <pre>
+          <code>{ EXAMPLES[selectedTopic].code }</code>
+        </pre>
+      </div>
+  }
+
   return (
     <div>
       <Header />
@@ -21,6 +44,16 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
